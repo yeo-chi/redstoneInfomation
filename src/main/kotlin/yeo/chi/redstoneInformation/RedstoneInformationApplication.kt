90cf94e0388d8c2eb86610721c9aa95d079d65d7
yeo_seung_hyeon <00www.black@gmail.com>
2023-12-11 20:22:12 +0900

    init

---
+++ src/main/kotlin/yeo/chi/redstoneInformation/RedstoneInformationApplication.kt
@@ -0,0 +1,11 @@
+package yeo.chi.redstoneInformation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedstoneInformationApplication
+
+fun main(args: Array<String>) {
+	runApplication<RedstoneInformationApplication>(*args)
+}
---
+++ src/test/kotlin/yeo/chi/redstoneInformation/RedstoneInformationApplicationTests.kt
@@ -0,0 +1,13 @@
+package yeo.chi.redstoneInformation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedstoneInformationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
